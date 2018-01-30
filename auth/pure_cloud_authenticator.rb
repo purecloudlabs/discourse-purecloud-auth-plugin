afdec1e946ded8d146f8746fa7cf2ec9cb937643
Tim Smith <tim.j.smith@genesys.com>
2018-01-30 11:18:40 -0700

    run init_settings inside lambda

--- auth/pure_cloud_authenticator.rb
+++ auth/pure_cloud_authenticator.rb
@@ -21,16 +21,14 @@ class PureCloudAuthenticator < ::Auth::OAuth2Authenticator
   end
 
   def register_middleware(omniauth)
-    init_settings
-
-    puts "Registering middleware"
-    puts "Client ID: " + SiteSetting.purecloud_client_id
     omniauth.provider :purecloud,
                       name: @provider_name,
                       setup: lambda {|env|
+                      	init_settings
+                      	
                         opts = env['omniauth.strategy'].options
-                        opts[:client_id] = SiteSetting.purecloud_client_id
-                        opts[:client_secret] = SiteSetting.purecloud_client_secret
+                        opts[:client_id] = @client_id
+                        opts[:client_secret] = @client_secret
 
                         opts[:client_options] = {
                           site: "https://login.#{@region}/"

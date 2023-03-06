1b455e65c45038f2831ce8ab2321a035f7e3d120
Ebenezer Osei <Ebenezer.Osei@genesys.com>
2023-03-06 14:53:33 -0500

    trying something

--- omniauth-genesys-cloud.rb
+++ omniauth-genesys-cloud.rb
@@ -2,12 +2,13 @@ require "omniauth-oauth2"
 
 module OmniAuth
   module Strategies
-    class GenesysCloud < OmniAuth::Strategies::OAuth2
+    class ::OmniAuth::Strategies::Oauth2Basic < ::OmniAuth::Strategies::OAuth2
       option :name, "genesysCloud"
 
       option :client_options, {
           authorize_url: '/oauth/authorize',
           token_url:     '/oauth/token'
+          site: 'https://login.mypurecloud.com'
       }
 
       option :authorize_options, [:scope]
